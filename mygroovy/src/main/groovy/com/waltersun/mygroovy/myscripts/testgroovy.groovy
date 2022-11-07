b996dc356d75c75216c942f03c237a2ad9895aad
道禹 Walter <sunwenhao.swh@alibaba-inc.com>
2022-11-07 20:35:11 +0800

    [*]hello world

---
+++ mygroovy/src/main/groovy/com/waltersun/mygroovy/myscripts/testgroovy.groovy
@@ -0,0 +1,12 @@
+package com.waltersun.mygroovy.myscripts
+
+/**
+ * @author 道禹 Walter
+ * @date 2022-11-07
+ */
+class Example {
+    static void main(String[] args) {
+        // Using a simple println statement to print output to the console
+        println('Hello World');
+    }
+}
