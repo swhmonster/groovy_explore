7844605afc1db306392f54c4ca19c345feb27549
道禹 Walter <sunwenhao.swh@alibaba-inc.com>
2022-11-07 20:32:34 +0800

    [*]build project

---
+++ mygroovy/src/main/groovy/com/waltersun/mygroovy/MygroovyApplication.groovy
@@ -0,0 +1,13 @@
+package com.waltersun.mygroovy
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class MygroovyApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MygroovyApplication, args)
+	}
+
+}
---
+++ mygroovy/src/test/groovy/com/waltersun/mygroovy/MygroovyApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.waltersun.mygroovy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MygroovyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
